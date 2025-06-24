0db8871f37c55373b0ff21f62c5ee28f70db174b
david-lambda <david.soto@lambdaanalytics.co>
2025-06-24 02:28:14 -0500

    feat(deploy):segundo intento

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/LAMBDA_doc_orquestador"
+  base: "/LAMBDA_doc_orquestador/"
+
 });
